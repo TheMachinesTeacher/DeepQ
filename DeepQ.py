1c4cefe65e7330faf8fbbdcb022d13fdb28871b1
Pillager225 <ryanloringcooper@gmail.com>
2017-07-17 14:59:27 -0700

    Fed data into q properly

--- DeepQ.py
+++ DeepQ.py
@@ -28,25 +28,25 @@ class ModelRun(Process):
     	self.gamma = tf.constant(.9, dtype=tf.float32, shape=(1,))
     	self.rHat = tf.placeholder(tf.float32, shape=(1,))
     	self.s = tf.placeholder(tf.float32, shape=()) #TODO figure this out
-    	self.sPrime = tf.placeholder(tf.float32, shape=()) #TODO figure this out
+    	self.sHatPrime = tf.placeholder(tf.float32, shape=()) #TODO figure this out
         self.q, self.qWeights, self.qBiases = makeModel(self.s, numActions)
-        self.qHat, self.qHatWeights, self.qHatBiases = makeModel(self.sPrime, numActions)
+        self.qHat, self.qHatWeights, self.qHatBiases = makeModel(self.sHatPrime, numActions)
     	self.rng = random.SystemRandom()
 
     def getTransistion(o): # TODO feed s into q when running it
-    	actionGraph = tf.argmax(self.q)
+
+    def squaredError(o):
+    	qResp = tf.argmax(self.q)
     	action = self.sess.run(actionGraph, feed_dict={self.s:o})
     	oPrime, reward, done, info = env.step(action)
-    	self.sPrime = tf.convert_to_tensor(oPrime)
-    	return self.s, action, reward, self.sPrime
-
-    def squaredError(q, qHat):
-    	transistion = self.getTransistion(q)
+    	transistion = (o, action, reward, oPrime)
     	transistionDB.append(transistion)
     	tranHat = transistionDB[self.rng.randint(0, len(transistionDB)-1)]
-        qResp = tf.reduce_max(q)
         qHatResp = tf.reduce_max(qHat)
-        y = tf.add(self.rHat, tf.multiply(self.gamma, qHatResp))
+        hatAction = self.sess.run(qHatResp, feed_dict={self.sHatPrime:tranHat[3]})
+
+
+        y = tf.add(self.rHat, tf.multiply(self.gamma, qHatResp)) # TODO self.rHat needs to be fed
         diff = tf.subtract(y, qResp)
         return tf.multiply(diff, diff)
 
@@ -63,6 +63,6 @@ class ModelRun(Process):
             oPrime, reward, done, info = env.step(0)
             while not done:
                 #env.render()
-                train_step.run(feed_dict={s:o, sPrime:oPrime, rHat:reward})
+                #train_step.run(feed_dict={s:o, sPrime:oPrime, rHat:reward})
             env.reset()
             env.close()
