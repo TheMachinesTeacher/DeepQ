749444c89e0ce93af6fa75dbe93cc4e8c4a9336c
Pillager225 <ryanloringcooper@gmail.com>
2017-07-18 15:50:29 -0700

    Incremental change

--- DeepQ.py
+++ DeepQ.py
@@ -3,6 +3,7 @@ import gym
 from gym import wrappers
 import tensorflow as tf
 from Model import *
+import random
 
 class ModelRun(Process):
 
@@ -18,10 +19,13 @@ class ModelRun(Process):
     qHat = None
     qHatWeights = None
     qHatBiases = None
-    rng = random.SystemRandom()
-    transistionDB = []
+    rng = None
+    transistionDB = None
+    DBIndex = 0
+    validDBEntries = 0
+    sess = None
 
-    def __init__(self, game, numActions):
+    def __init__(self, game, numActions, DBSize):
         super(ModelRun, self).__init__()
         self.game = game
         self.numActions = numActions
@@ -32,21 +36,25 @@ class ModelRun(Process):
         self.q, self.qWeights, self.qBiases = makeModel(self.s, numActions)
         self.qHat, self.qHatWeights, self.qHatBiases = makeModel(self.sHatPrime, numActions)
     	self.rng = random.SystemRandom()
+    	self.transistionDB = [[]]*DBSize
 
-    def getTransistion(o): # TODO feed s into q when running it
+    def addToDB(self, tran):
+    	transistionDB[DBIndex] = tran
+    	DBIndex = 0 if DBIndex == len(self.transistionDB)-1 else DBIndex+1
+    	if validDBEntries < len(self.transistionDB):
+    		validDBEntries += 1
 
-    def squaredError(o):
+    def squaredError(self, o):
     	qResp = tf.argmax(self.q)
     	action = self.sess.run(actionGraph, feed_dict={self.s:o})
     	oPrime, reward, done, info = env.step(action)
-    	transistion = (o, action, reward, oPrime)
-    	transistionDB.append(transistion)
-    	tranHat = transistionDB[self.rng.randint(0, len(transistionDB)-1)]
+    	self.addToDB((o, action, reward, oPrime))
+    	tranHat = transistionDB[self.rng.randint(0, validDBEntries-1)]
         qHatResp = tf.reduce_max(qHat)
         hatAction = self.sess.run(qHatResp, feed_dict={self.sHatPrime:tranHat[3]})
 
 
-        y = tf.add(self.rHat, tf.multiply(self.gamma, qHatResp)) # TODO self.rHat needs to be fed
+        y = tf.add(self.rHat, tf.multiply(self.gamma, hatAction)) # TODO self.rHat needs to be fed
         diff = tf.subtract(y, qResp)
         return tf.multiply(diff, diff)
 
@@ -54,15 +62,15 @@ class ModelRun(Process):
         done = False
         env = gym.make(game)
         env = wrappers.Monitor(env, "/tmp/gym-results")
-        with tf.Session() as sess:
-            with tf.name_scope('loss'):
-                loss = self.squaredError(q, qHat) 
-            with tf.name_scope('train'):
-                train_step = tf.train.AdamOptimizer(learning_rate=0.001, beta1=0.9, beta2=0.999, epsilon=1e-8).minimize(loss)
-            o, reward, done, info = env.step(0)
-            oPrime, reward, done, info = env.step(0)
-            while not done:
-                #env.render()
-                #train_step.run(feed_dict={s:o, sPrime:oPrime, rHat:reward})
-            env.reset()
-            env.close()
+        sess = tf.Session()
+        o, reward, done, info = env.step(0)
+        with tf.name_scope('loss'):
+            loss = self.squaredError(o) 
+        with tf.name_scope('train'):
+            train_step = tf.train.AdamOptimizer(learning_rate=0.001, beta1=0.9, beta2=0.999, epsilon=1e-8).minimize(loss)
+        oPrime, reward, done, info = env.step(0)
+        #while not done:
+            #env.render()
+            #train_step.run(feed_dict={s:o, sPrime:oPrime, rHat:reward})
+        env.reset()
+        env.close()
